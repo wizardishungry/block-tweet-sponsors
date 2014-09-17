67dc4938e16dcfbd7fb43d876f7ba6090d802c7f
Jon Williams <jonathan.williams@gmail.com>
2014-09-17 14:52:28 -0400

    First block of a shit eating sponsored tweet

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 block-tweet-sponsors
 ====================
 
-delete 
+Written in PHP. I'm sorry.
+
+# git submodule init && git submodule update
+# cp config.php{.dist,}
+# Create api stuff from https://apps.twitter.com and put it in config.php
+# php block.php
+
+Eat shit sponsorers!
---
+++ block.php
@@ -0,0 +1,41 @@
+<?php
+define('TEST', true);
+require_once('twitteroauth/twitteroauth/twitteroauth.php');
+require_once('config.php');
+
+$path = dirname(__FILE__);
+$twitter = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET);
+
+if(!TEST) {
+
+    $twitter->host = "https://twitter.com/";
+    $tweets = $twitter->get("/i/timeline", array('include_available_features'=>1, 'include_entities'=>1, 'last_note_ts'=>'1410970560','max_position'=>'512272361681190913'));
+    $tweets = $tweets->items_html;
+}
+else {
+    $tweets = file_get_contents("$path/test.txt");
+}
+
+
+$dom = DOMDocument::loadHTML($tweets);
+$finder = new DomXPath($dom);
+$classname="promoted-tweet";
+$nodeList = $finder->query("//*[contains(concat(' ', normalize-space(@class), ' '), ' $classname ')]");
+$blocklist = array();
+foreach ($nodeList as $node) {
+    $name = $node->getAttribute("data-screen-name");
+    $follow = $node->getAttribute("data-you-follow");
+    $following = $follow=="true";
+    $block = $node->getAttribute("data-you-block");
+    $blocking = $block=="true";
+    echo "$name ".($following?' following...':''). ($blocking?' blocking...':'')."\n";
+    if(!$following && !$blocking) {
+        $blocklist[$name] = $name;
+    }
+}
+
+$twitter->host = "https://api.twitter.com/1.1/";
+foreach($blocklist as $name) {
+    echo "blocking $name...\n";
+    $result = $twitter->post('blocks/create',array('screen_name' => $name));
+}
